65d25c796f66f76bf95dcd48d56e3d1f673e7964
Olli K <gildean@salaliitto.com>
2015-03-05 14:41:38 +0200

    Update to version 1

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
-flowdock-stream
-===============
+flowdock-stream v.1
+===================
 
 A node module for streaming flowdock flows.
+NOTE: for the older API, check version 0.1.3.
 
 Installation
 ------------
@@ -13,20 +14,21 @@ Usage
 
 ```js
 var FlowdockStream = require('flowdock-stream');
-var flowdockstream = FlowdockStream.createClient('organization', 'flow', 'apikey');
+var flowdockstream = FlowdockStream.createClient('organization', ['flow', 'another'], 'personal-apikey');
 
 flowdockstream.on('ready', function () {
-    console.log('flowdockstream is ready, current users in the flow:', flowdockstream.flowUsers);
+    console.log('flowdockstream is ready, current users in the flows:\r\n', flowdockstream.flows);
 });
 
 flowdockstream.on('data', function flowDockEventHandler(data) {
     if (data.event === 'message') {
-        var from = (data.user) ? flowdockstream.flowUsers[data.user] : null;
+        var from = (data.user) ? flowdockstream.flows[data.flow].users[data.user] : null;
         console.log('a message from', from, data.content);
     } else if (data.event === 'join') {
-        flowdockstream.getUsers(function setUsers(err, users) {
+        flowdockstream.getUsers(flowdockstream.flows[data.flow].name, function setUsers(err, users) {
             if (err) return flowdockstream.emit('error', err);
-            flowdockstream.flowUsers = users;
+            flowdockstream.flows[data.flow].users = users;
+            flowdockstream.send(flowdockstream.flows[data.flow].name, 'Hello ' + users[data.user]);
         });
     }
 });
@@ -42,26 +44,33 @@ Public methods
 A stream is created with the only exported function as follows:
 
 
-__FlowdockStream.createClient(__ *organization*, *flow*, *apikey* __)__
+__FlowdockStream.createClient(__ *organization*, *flows*, *apikey* __)__
     
-  - all three arguments are mandatory, the apikey is a __personal apikey__, not an apikey of a flow (the bot accesses the REST-API that requires the personal key)
+  - all three arguments are mandatory, the apikey is a __personal apikey__, not an apikey of a flow (the module accesses the REST-API that requires the personal key)
+  - the `flows` argument can be either a string (for a sinlge flow) or an array (for multiple flows)
   - the stream will emit a `ready` event once the stream is about to start emitting data and the `flowUsers` property has been set
 
 The stream itself is a readable node.js stream, but added are a couple of methods for convenience:
 
-__flowdockStream.flowUsers__
+__flowdockStream.flows__
     
-  - Object that contains the ids and nicks of all the users in the flow at the time of joining, set once for your convenience
+  - Object that contains the names and urikeys as well as all of the users in the flow at the time of joining.
 
 
-__flowdockStream.getUsers(__ *callback* __)__
+__flowdockStream.getFlows(__ *callback* __)__
     
-  - Function that takes a *callback* which returns two arguments *error* (Error), and *users* (Object)
+  - Function that takes a *callback*-function which returns two arguments *error* (Error), and *flows* (Object), this also gets all of the users for each flow.
 
 
-__flowdockStream.send(__ *message*, [*commentToId*], [*callback*] __)__
+__flowdockStream.getUsers(__ *flow*, *callback* __)__
     
-  - Function that can be used to send messages to the flow. Optionally you can pass an message-id as the second argument to send the message as a comment to a previous message. Also takes an optional callback which is passed to the underlying *request* module.
+  - Function that takes the name of the flow *flow* and a *callback*-function which returns two arguments *error* (Error), and *users* (Object)
+
+
+__flowdockStream.send(__ *flow*, *message*, [*commentToId*], [*callback*] __)__
+
+  - Function that can be used to send a `message` to the `flow` (you can pick the flow-name from the flows-object by the id in an event-callback). Optionally you can pass an message-id as the third argument to send the message as a comment to a previous message. Also takes an optional callback which is passed to the underlying *request* module.
+
 
 License
 -------
--- index.js
+++ index.js
@@ -1,28 +1,38 @@
 var request = require('request');
 var JSONParseStream = require('./jsonparser.js');
 var PassThrough = require('stream').PassThrough;
-require('util').inherits(StreamingClient, PassThrough);
 
+function parameterized(flowName) {
+    return flowName.toLowerCase().replace(/ /g, '-');
+}
+
+function nickReducer(obj, user) {
+    if (user.hasOwnProperty('id') && user.hasOwnProperty('nick')) obj[user.id] = user.nick;
+    return obj;
+}
+
+require('util').inherits(StreamingClient, PassThrough);
 function StreamingClient(org, flow, apikey) {
     if (!(this instanceof StreamingClient)) return new StreamingClient(org, flow, apikey);
     PassThrough.call(this);
     this._writableState.objectMode = true;
     this._readableState.objectMode = true;
     this.org = org.toLowerCase();
-    this.flow = flow.toLowerCase();
     this.apikey = apikey;
     this.apiuri = 'https://api.flowdock.com/flows';
     this.streamuri = 'https://stream.flowdock.com/flows';
-    this.urikey =  this.org + '/' + this.flow;
+    this.flow = ('string' === typeof flow) ? [parameterized(flow)] : flow.map(parameterized);
+    var self = this;
+    this.flowMap = {};
 }
 
 // "private" methods
 StreamingClient.prototype._streamParser = new JSONParseStream();
-StreamingClient.prototype._getSendOpts = function _getSendOpts(message, messageId) {
+StreamingClient.prototype._getSendOpts = function _getSendOpts(flow, message, messageId) {
     var self = this;
     var comment = (messageId) ? '/' + messageId + '/comments' : '';
     return {
-        uri: self.apiuri + '/' + self.urikey + '/messages' + comment,
+        uri: self.apiuri + '/' + self.flows[flow.toLowerCase()].urikey + '/messages' + comment,
         method: 'POST',
         json: {
             event: (messageId) ? 'comment' : 'message',
@@ -33,48 +43,76 @@ StreamingClient.prototype._getSendOpts = function _getSendOpts(message, messageI
 
 StreamingClient.prototype._init = function _init() {
     var self = this;
-    this.getUsers(function setUsers(err, users) {
-        if (err) self.emit('error', err);
-        self.flowUsers = users;
-        request.get(self.streamuri + '?filter=' + self.urikey).auth(self.apikey, '', true).pipe(self._streamParser).pipe(self);
-        self.emit('ready');
+    this.getFlows(function gotFlows(err, flows) {
+        if (err) return self.emit('error', err);
+        self.flows = flows;
+        return self._createStreams();
     });
     return this;
 };
 
-// public methods
-StreamingClient.prototype.flowUsers = {};
-StreamingClient.prototype.send = function send(message, messageId, cb) {
+StreamingClient.prototype._createStreams = function _createStreams() {
     var self = this;
-    var options = this._getSendOpts(message, messageId);
+    var filter = '?filter=' + Object.keys(this.flows).map(function (flow) { return self.flows[flow].urikey; }).join(',');
+    var url = this.streamuri + filter
+    request.get(url).auth(self.apikey, '', true).pipe(self._streamParser).pipe(self);
+    this.emit('ready');
+};
+
+// public methods
+StreamingClient.prototype.send = function send(flow, message, messageId, cb) {
+    var self = this;
+    if ('function' === typeof messageId) cb = messageId;
+    var options = this._getSendOpts(this.flowMap[parameterized(flow)], message, messageId);
     return request(options).auth(self.apikey, '', true, cb);
 };
 
-StreamingClient.prototype.getUsers = function getUsers(callback) {
+StreamingClient.prototype.getUsers = function getUsers(flowName, callback) {
     var self = this;
     var jsonParser = new JSONParseStream();
     jsonParser.once('data', function getUserData(data) {
         jsonParser.removeAllListeners();
         if (data.message || data.error && data.error.message) return callback(new Error(data.message || data.error.message));
-        return callback(null, data.reduce(function nickReducer(obj, user) {
-            if (user.hasOwnProperty('id') && user.hasOwnProperty('nick')) obj[user.id] = user.nick;
-            return obj;
-        }, {}));
+        return callback(null, data.reduce(nickReducer, {}));
     });
     jsonParser.once('error', function errorHandler(err) {
         jsonParser.removeAllListeners();
         return callback(err, null);
     });
-    return request.get(self.apiuri + '/' + self.urikey + '/users').auth(self.apikey, '', true).pipe(jsonParser);
+    return request.get(self.apiuri + '/' + self.flows[self.flowMap[parameterized(flowName)]].urikey + '/users').auth(self.apikey, '', true).pipe(jsonParser);
+};
+
+StreamingClient.prototype.getFlows = function getUsers(callback) {
+    var self = this;
+    var jsonParser = new JSONParseStream();
+    jsonParser.once('data', function getUserData(data) {
+        jsonParser.removeAllListeners();
+        if (data.message || data.error && data.error.message) return callback(new Error(data.message || data.error.message));
+        var flows = data.reduce(function flowReducer(obj, flow) {
+            var flowName = flow['parameterized_name'];
+            if (self.flow.indexOf(flowName) !== -1) {
+                obj[flow.id] = {
+                    users: flow.users.reduce(nickReducer, {}),
+                    name: flowName,
+                    urikey: self.org + '/' + flowName
+                };
+                self.flowMap[flowName] = flow.id;
+            }
+            return obj;
+        }, {});
+        return callback(null, flows);
+    });
+    jsonParser.once('error', function errorHandler(err) {
+        jsonParser.removeAllListeners();
+        return callback(err);
+    });
+    return request.get(self.apiuri + '?users=1').auth(self.apikey, '', true).pipe(jsonParser);
 };
 
 var flowdock = {
     createClient: function createClient(org, flow, apikey) {
         if (!org || !flow || !apikey) throw new Error('missing arguments; org, flow and apikey required');
-        var sclient = new StreamingClient(org, flow, apikey);
-        setImmediate(function initStream() {
-            sclient._init();
-        });
+        var sclient = new StreamingClient(org, flow, apikey)._init();
         return sclient;
     }
 };
